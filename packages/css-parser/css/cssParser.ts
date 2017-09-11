ff9a5775bf1cd26b22ae20c0310f2f6a077c6e61
David Kudera <kudera.d@gmail.com>
2017-09-11 16:01:35 +0200

    CSSParser: upgrade css-tree

--- packages/css-parser/css/cssParser.ts
+++ packages/css-parser/css/cssParser.ts
@@ -9,15 +9,14 @@ export class CSSParser
 	public parse(code: string): Array<n.CSSNodeSelector>
 	{
 		const ast = csstree.parse(code, {
-			//parseSelector: false,			// todo: uncomment when https://github.com/csstree/csstree/issues/56 is fixed
+			parseSelector: false,
 			parseValue: false,
 		});
 
 		let result: Array<n.CSSNodeSelector> = [];
 
 		csstree.walkRules(ast, (node) => {
-			const selector = csstree.translate(node.selector);
-			const current = new n.CSSNodeSelector(selector);
+			const current = new n.CSSNodeSelector(node.selector.value.trim());
 
 			csstree.walkDeclarations(node.block, (rule) => {
 				current.rules.push(new n.CSSNodeRule(rule.property.trim(), rule.value.value.trim(), rule.important));
