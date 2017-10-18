949baa646031062dd8447184260ca2d3a1d833cd
David Kudera <kudera.d@gmail.com>
2017-10-18 22:16:27 +0200

    Core: add list of children directives into ChildrenDirectivesStorage

--- packages/core/directives/childrenDirectivesStorage.ts
+++ packages/core/directives/childrenDirectivesStorage.ts
@@ -9,4 +9,24 @@ export class ChildrenDirectivesStorage<T>
 
 	public remove = new EventEmitter<T>();
 
+	private _children: Array<T> = [];
+
+
+	constructor()
+	{
+		this.add.subscribe((child: T) => {
+			this._children.push(child);
+		});
+
+		this.remove.subscribe((child: T) => {
+			this._children.splice(this._children.indexOf(child), 1);
+		});
+	}
+
+
+	get children(): Array<T>
+	{
+		return this._children;
+	}
+
 }
