cbe3b9407e276113cb002e47fd8563ea9d6b48a9
David Kudera <kudera.d@gmail.com>
2017-10-29 09:43:37 +0100

    Fix examples

--- examples/examples/loop/app/bootstrap.ts
+++ examples/examples/loop/app/bootstrap.ts
@@ -3,11 +3,9 @@ import 'reflect-metadata';
 
 import {Container} from '@slicky/di';
 import {Application} from '@slicky/application';
-import {PlatformServer} from '@slicky/platform-server';
+import {PlatformInline} from '@slicky/platform-inline';
 //import {PlatformBrowser} from '@slicky/platform-browser';
 import {LoopApp} from './loopApp';
-
-import {APP_TEMPLATES_FACTORY} from '../aot/app-templates-factory';
 
 
 const container = new Container;
@@ -19,7 +17,7 @@ const app = new Application(container, {
 });
 
 
-const platform = new PlatformServer(APP_TEMPLATES_FACTORY);
+const platform = new PlatformInline;
 //const platform = new PlatformBrowser;
 
 platform.run(app, '#app');
